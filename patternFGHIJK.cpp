8e3ebc77dc1e5582fa9da83e6af69ac34ccf9b12
karinasayta8 <90768941+karinasayta8@users.noreply.github.com>
2023-09-25 23:26:30 +0530

    Add files via upload

---
+++ patternFGHIJK.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int row = 1;row <= 5;row++){
+         for(char i ='F'; i <= 'K'; i++){
+            cout<<i<<" ";
+         }
+         cout<<endl;
+    }
+}
